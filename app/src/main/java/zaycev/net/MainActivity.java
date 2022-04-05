eaceac9f51cc11509db24d75d2acb4299c618b77
SaDiSST <SaDiSST@users.noreply.github.com>
2022-04-05 18:58:12 +0500

    deleted unused imports and static variables in MainActivity class

--- app/src/main/java/zaycev/net/MainActivity.java
+++ app/src/main/java/zaycev/net/MainActivity.java
@@ -2,16 +2,11 @@ package zaycev.net;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.SharedPreferences;
-import android.widget.Toast;
 import android.os.Bundle;
 
 import zaycev.net.interfaces.DiscountsApplier;
 
 public class MainActivity extends AppCompatActivity implements DiscountsApplier {
-    private static SharedPreferences sharedPreferences;
-    private static String sharedPreferencesFile;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
